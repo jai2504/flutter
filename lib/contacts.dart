93cbfcbda263da45345933fde8b69a343f59c9d5
jai2504 <rahejajai123@gmail.com>
2020-06-16 11:18:34 +0530

    new commit

--- lib/contacts.dart
+++ lib/contacts.dart
@@ -30,51 +30,96 @@ class _ContactslisState extends State<Contactslis> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).accentColor,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              setState(() {
-                Navigator.pop(context);
-              });
-            }),
-        title: Text(
-          'Select Contact',
-          style: TextStyle(color: Colors.white),
-        ),
-        bottom:
-            PreferredSize(child: Text('$len Contacts'), preferredSize: null),
-        actions: <Widget>[
-          Icon(
-            Icons.search,
-            color: Colors.white,
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65),
+        child: AppBar(
+          backgroundColor: Theme.of(context).primaryColor,
+          leading: Padding(
+            padding: EdgeInsets.only(top: 15),
+            child: IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                }),
           ),
-          Padding(padding: const EdgeInsets.symmetric(horizontal: 7)),
-          Icon(
-            Icons.more_vert,
-            color: Colors.white,
-          )
-        ],
-      ),
-      body: Container(
-        margin: EdgeInsets.all(24),
-        child: Column(
-          children: <Widget>[
-            ListView.builder(
-                shrinkWrap: true,
-                itemCount: len,
-                itemBuilder: (context, index) {
-                  Contact c = contact[index];
-                  return ListTile(
-                    title: Text(c.displayName),
-                    subtitle: Text(c.phones.elementAt(0).value),
-                  );
-                })
+          title: Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              'Select Contact',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: null,
+            child: Container(
+              margin: EdgeInsets.only(left: 72, bottom: 5),
+              height: 18.0,
+              alignment: Alignment.bottomLeft,
+              child: Text('$len Contacts'),
+            ),
+          ),
+          actions: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+            Padding(padding: const EdgeInsets.symmetric(horizontal: 5)),
+            Padding(
+              padding: EdgeInsets.only(top: 15),
+              child: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ),
+            )
           ],
         ),
       ),
+       body: Column(children: <Widget>[
+         ListTile(
+           leading: CircleAvatar(
+             child:Icon(Icons.group_add,color: Colors.white,),
+             backgroundColor: Theme.of(context).accentColor,
+           ),
+
+         ),
+         ListTile(
+             leading: CircleAvatar(
+               radius: 25,
+             child:
+             Center(child: Icon(Icons.person_add,color: Colors.white,)),
+             backgroundColor: Theme.of(context).accentColor,
+           ),
+         ),
+        Expanded(
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: len,
+              itemBuilder: (context, index) {
+                Contact c = contact[index];
+                return Column(
+                  children: <Widget>[
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor: Colors.grey,
+                      ),
+                      title: Text(c.displayName),
+                      subtitle: Text(c.phones.elementAt(0).value),
+                    )
+                  ],
+                );
+              }
+              ),
+        ),
+      ]),
     );
   }
 }
+/*
+
+     */
